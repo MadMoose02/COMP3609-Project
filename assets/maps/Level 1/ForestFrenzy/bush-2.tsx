1a2aeedaef3025b806c5c1bd3dfa8f652ea2d36a
Shay B <shaniahbaldeo02@gmail.com>
2024-04-21 02:15:26 -0400

    level 1 completed

---
+++ assets/maps/Level 1/ForestFrenzy/bush-2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="bush-2" tilewidth="43" tileheight="22" tilecount="1" columns="1">
+ <image source="../../Downloads/forest-frenzy-assets/3 Objects/Nature/bush-2.png" width="43" height="22"/>
+</tileset>
