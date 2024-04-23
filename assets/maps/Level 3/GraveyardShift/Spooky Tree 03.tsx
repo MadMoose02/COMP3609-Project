02338751eb76aed2d6b7f71e7784c4e53b48f0a5
Shay B <shaniahbaldeo02@gmail.com>
2024-04-23 08:00:04 -0400

    level 3 completed

---
+++ assets/maps/Level 3/GraveyardShift/Spooky Tree 03.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Spooky Tree 03" tilewidth="128" tileheight="128" tilecount="4" columns="2">
+ <image source="../../Downloads/Graveyard Shift/Props/Spooky Tree 03.png" width="256" height="256"/>
+</tileset>
---
+++ assets/maps/Level 3/GraveyardShift/TombStone 4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TombStone 4" tilewidth="84" tileheight="120" tilecount="1" columns="1">
+ <image source="../../Downloads/Graveyard Shift/Props/TombStone 4.png" width="84" height="120"/>
+</tileset>
