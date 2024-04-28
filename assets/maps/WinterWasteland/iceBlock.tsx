11abc8e99b6756edf82d5863e9372e2f970cdf42
Keshan Moosai <keshanmoosai@gmail,com>
2024-04-27 22:55:41 -0400

    fixed muti-tile issues and inconsistent layer structure

--- assets/maps/WinterWasteland/iceBlock.tsx
+++ assets/maps/WinterWasteland/iceBlock.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="iceBlock" tilewidth="70" tileheight="70" tilecount="1" columns="1">
- <image source="Assets/kenney_platformer-art-winter/Tiles/IceBlock/iceBlock.png" width="70" height="70"/>
+ <image source="Assets/Tiles/IceBlock/iceBlock.png" width="70" height="70"/>
 </tileset>
--- assets/maps/WinterWasteland/treeTrunkBottomAlt3.tsx
+++ assets/maps/WinterWasteland/treeTrunkBottomAlt3.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="treeTrunkBottomAlt3" tilewidth="70" tileheight="70" tilecount="1" columns="1">
- <image source="Assets/kenney_platformer-art-winter/Tiles/Nature/treeTrunkBottomAlt3.png" width="70" height="70"/>
+ <image source="Assets/Tiles/Nature/treeTrunkBottomAlt3.png" width="70" height="70"/>
 </tileset>
