dac8e3ef115b451246e486b5e6f71d9245d2ba41
Keshan Moosai <keshanmoosai@gmail.com>
2024-04-28 19:05:14 -0400

    added overload constructor for defaults

--- src/Entity/Coin.java
+++ src/Entity/Coin.java
@@ -2,9 +2,14 @@ package Entity;
 
 public class Coin extends Collectible{
     private int numCoins;
+
+    public Coin(int x, int y, Player player) {
+        super("coin", x, y, 40, 40, player);
+        this.numCoins = 0;
+    }
     
-    public Coin(String imgName, int x, int y, int width, int height, Player player) {
-        super(imgName, x, y, width, height, player);
+    public Coin(int x, int y, int width, int height, Player player) {
+        super("coin", x, y, width, height, player);
         this.numCoins = 0;
     }
 
